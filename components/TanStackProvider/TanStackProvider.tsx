101e8e938844cee037dd8e5e5737c95dd6620b1a
Nataliia_Romanchenko <rnataliia36gmail.com>
2026-05-15 14:36:05 +0200

    fix TanStackProvider

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-export default function Providers({
-  children,
-}: {
+interface Props {
   children: React.ReactNode;
-}) {
+}
+
+export default function TanStackProvider({
+  children,
+}: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
